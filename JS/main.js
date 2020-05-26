81a5018521c90956311a83be075601f70f3414a4
PrameshShrestha <59064718+pramesh025@users.noreply.github.com>
2020-05-26 12:40:24 +0545

    2.1

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,37 @@
-$('.option').on('mouseenter', (event) => {
-    $(event.currentTarget).addClass('hover');
-}).on('mouseleave', (event) => {
-    $(event.currentTarget).removeClass('hover');
-}).on('click', (event) => {
-    $('.options').children().removeClass('selected');
-    $(event.currentTarget).addClass('selected');
+$(document).ready(() => {
+    let username = '';
+    $('.username').on('keyup', () => {
+        username = $('.username').val();
+    })
+
+    function submit() {
+        if (username === '') {
+            alert('Please enter a Username');
+        } else {
+            $('.log_in').addClass('d-none');
+            $('.quiz_interface').removeClass('d-none');
+
+            $('#name').html('Hello ' + username + '! ');
+        }
+    }
+    $('.login_button').on('click', submit)
+    $('.username').keyup((event) => {
+        if (event.keyCode === 13)
+            submit();
+    })
+    $('.logout').on('click', () => {
+        $('.log_in').removeClass('d-none');
+        $('.quiz_interface').addClass('d-none');
+        username = '';
+        $('.username').val('');
+    })
+
+    $('.option').on('mouseenter', (event) => {
+        $(event.currentTarget).addClass('hover');
+    }).on('mouseleave', (event) => {
+        $(event.currentTarget).removeClass('hover');
+    }).on('click', (event) => {
+        $('.options').children().removeClass('selected');
+        $(event.currentTarget).addClass('selected');
+    });
 });
